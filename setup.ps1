66565200659ca635bc7fe67f6503bd7ed663e006
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-04-22 08:57:28 -0700

    Move git configure into personal dsc yaml.

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,3 @@
-# Sample of a personal automation script
-git config --global user.name "Anthony Cangialosi [MS]"
-git config --global user.email 5457010+acangialosi@users.noreply.github.com
-
 # Set dark mode
 winget configure '.\personal-dev-env.yaml' --accept-configuration-agreements
 
