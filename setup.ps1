2d519cabe9da36cae89eec9b3aa4e2b7dfcad05b
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-04-22 09:36:43 -0700

    Switch setup to use winget cmdlet

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,4 @@
-# Set dark mode
-winget configure '.\personal-dev-env.yaml' --accept-configuration-agreements
-
-# Setup OMP
-oh-my-posh font install --user CascadiaCode
-Set-Content -Path $PROFILE -Value 'oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression'
-Install-Module -Name Terminal-Icons -Repository PSGallery -Force    
-Import-Module -Name Terminal-Icons
+# Install the Winget Cmndlet required for enabling windows features and system level installation
+install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease
+#winget configure '.\personal-dev-env.yaml' --accept-configuration-agreements
+get-WinGetConfiguration -file .\windows\windowsfeatures.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
---
+++ terminal/configure-omp.ps1
@@ -0,0 +1,5 @@
+# Setup OMP
+oh-my-posh font install --user CascadiaCode
+Set-Content -Path $PROFILE -Value 'oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression'
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force    
+Import-Module -Name Terminal-Icons
