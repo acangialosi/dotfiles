c309b6515a52b6c74f3d17f111c0ceb1b071388b
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-04-22 19:14:32 -0700

    Rename and redirect to use ready-for-me scripts.

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,5 @@
+# Run from an elevated PowerShell session
 # Install the Winget Cmndlet required for enabling windows features and system level installation
 install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease
 #winget configure '.\personal-dev-env.yaml' --accept-configuration-agreements
-get-WinGetConfiguration -file .\windows\windowsfeatures.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
+get-WinGetConfiguration -file .\ready-for-me.dsc.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
