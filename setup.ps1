343aeb04770d39f2e18c5d0ac335767dd195567f
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-05-06 22:24:56 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,5 +4,6 @@ winget install Microsoft.PowerShell
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 # Install the Winget Cmndlet required for enabling windows features and system level installation
 install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 # Run the dsc configuration using winget cmdlet. Winget.exe cannot run as system or install Windows Optional Features
 get-WinGetConfiguration -file $PSScriptRoot\ready-for-me.dsc.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
