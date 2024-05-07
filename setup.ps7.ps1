7cf6565116a319c64f648dfad45431228072bbfd
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-05-06 23:00:02 -0700

    Create setup.ps7.ps1

---
+++ setup.ps7.ps1
@@ -0,0 +1,7 @@
+# Run from an elevated PowerShell session
+# Install the Winget Cmndlet required for enabling windows features and system level installation
+set-psrepository -name PSGallery -InstallationPolicy Trusted
+install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease -AcceptLicense
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+# Run the dsc configuration using winget cmdlet. Winget.exe cannot run as system or install Windows Optional Features
+get-WinGetConfiguration -file $PSScriptRoot\ready-for-me.dsc.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
