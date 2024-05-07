6fc5f77aff14e96e9d59ebe49e6b41505da10b56
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-05-06 17:01:09 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,7 @@
 # Run from an elevated PowerShell session
+# Install Powershell
+winget install Microsoft.PowerShell
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 # Install the Winget Cmndlet required for enabling windows features and system level installation
 install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease
 #winget configure '.\personal-dev-env.yaml' --accept-configuration-agreements
