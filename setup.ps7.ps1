5468877435eafac9ef4e9cb3491ddc9ff4c3d731
Anthony Cangialosi [MS] <5457010+acangialosi@users.noreply.github.com>
2024-05-15 07:59:15 -0700

    move ready-for-me.dsc.yaml to .configurations folder.

--- setup.ps7.ps1
+++ setup.ps7.ps1
@@ -4,4 +4,4 @@ set-psrepository -name PSGallery -InstallationPolicy Trusted
 install-Module -Name Microsoft.WinGet.Configuration -AllowPrerelease -AcceptLicense
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 # Run the dsc configuration using winget cmdlet. Winget.exe cannot run as system or install Windows Optional Features
-get-WinGetConfiguration -file $PSScriptRoot\ready-for-me.dsc.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
+get-WinGetConfiguration -file $PSScriptRoot\.configurations\ready-for-me.dsc.yaml | Invoke-WinGetConfiguration -AcceptConfigurationAgreements
